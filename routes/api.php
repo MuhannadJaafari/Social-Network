b3c8f55ec5ab1a2bb281fdf1c034b3c15e23677e
muhannad <mohanad.j45@gmail.com>
2021-06-05 22:53:52 +0300

    api's added

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//public routes
+Route::post('/register',[UserController::class,'store']);
+Route::post('/login',[LoginController::class,'login']);
+Route::get('/{user_id}',[UserController::class,'show']);
+Route::get('/{user_id}/profilePic',[UserController::class,'getProfilePic']);
+Route::get('/{user_id}/posts',[UserController::class,'getPosts']);
+Route::get('/{user_id}/friends',[UserController::class,'getFriends']);
+Route::get('/{user_id}/photos',[UserController::class,'getPhotos']);
+Route::get('{post_id}/comments',[PostController::class,'getComments']);
+Route::get('{post_id}/likes',[PostController::class,'getLikes']);
+Route::get('{post_id}/sharers',[PostController::class,'getSharers']);
+//protected routes
+Route::delete('/deactivate',[UserController::class,'destroy']);
+Route::put('/update',[UserController::class,'destroy']);
+Route::post('/newPost',[PostControllr::class,'store']);
+Route::put('/updatePost',[PostControllr::class,'update']);
+Route::delete('/deletePost',[PostController::class,'destroy']);
+Route::post('/newComment',[CommnetController::class,'store']);
+Route::put('/updateComment',[CommnetController::class,'update']);
+Route::delete('/deleteComment',[CommnetController::class,'destroy']);
+Route::post('newHashtag',[HashtagController::class,'store']);
+Route::post('newLike',[LikeController::class,'store']);
+Route::put('updateLike',[LikeController::class,'update']);
+Route::delete('deleteLike',[LikeController::class,'destroy']);
+Route::get('/newsFeed',[PostController::class,'getNewsFeed']);
+Route::post('/{post}/viewed',[PostController::class,'viewPost']);//for newsFeed algo
+Route::put('/updateProfilePic',[PhotoController::class,'updateProfilePic']);
+Route::delete('/deleteProfilePic',[PhotoController::class,'deleteProfilePic']);
+
