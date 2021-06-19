0547b1a3354f0cd6b91ead1895752fcc2eeb258d
AmerThallaj <amoorthallaj99@gmail.com>
2021-06-19 21:33:14 +0300

    page and group test

---
+++ app/Models/Group.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Users\Username;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+    public function posts()
+    {
+        return $this->morphMany(Post::class,'postable');
+    }
+    public function username()
+    {
+        return $this->morphOne(Username::class,'useable');
+    }
+    public function roles(){
+        return $this->morphMany(Role::class,'roleable_type');
+    }
+}
---
+++ app/Models/Page.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Users\User;
+use App\Models\Users\Username;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    use HasFactory;
+    public function posts()
+    {
+        return $this->morphMany(Post::class,'postable');
+    }
+    public function username()
+    {
+        return $this->morphOne(Username::class,'useable');
+    }
+    public function roles(){
+        return $this->morphMany(Role::class,'roleable_type');
+    }
+
+}
